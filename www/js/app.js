878dc4b847165f006a48b56f13251f94f6c117b9
Alexandru Grimaliuc <grimaliuc.alexandru97@gmail.com>
2023-02-15 10:31:49 +0200

    release v1.0.1

--- www/js/app.js
+++ www/js/app.js
@@ -2,45 +2,46 @@ var $ = Dom7;
 
 var device = Framework7.getDevice();
 var app = new Framework7({
-  name: 'Auto Update App', // App name
-  theme: 'auto', // Automatic theme detection
-  el: '#app', // App root element
+    name: 'Auto Update App', // App name
+    theme: 'auto', // Automatic theme detection
+    el: '#app', // App root element
+    version: '1.0.1',
+    id: 'io.framework7.myapp', // App bundle ID
+    // App store
+    store: store,
+    // App routes
+    routes: routes,
 
-  id: 'io.framework7.myapp', // App bundle ID
-  // App store
-  store: store,
-  // App routes
-  routes: routes,
 
-
-  // Input settings
-  input: {
-    scrollIntoViewOnFocus: device.cordova && !device.electron,
-    scrollIntoViewCentered: device.cordova && !device.electron,
-  },
-  // Cordova Statusbar settings
-  statusbar: {
-    iosOverlaysWebView: true,
-    androidOverlaysWebView: false,
-  },
-  on: {
-    init: function () {
-      var f7 = this;
-      if (f7.device.cordova) {
-        // Init cordova APIs (see cordova-app.js)
-        cordovaApp.init(f7);
-      }
+    // Input settings
+    input: {
+        scrollIntoViewOnFocus: device.cordova && !device.electron,
+        scrollIntoViewCentered: device.cordova && !device.electron,
     },
-  },
+    // Cordova Statusbar settings
+    statusbar: {
+        iosOverlaysWebView: true,
+        androidOverlaysWebView: false,
+    },
+    on: {
+        init: function () {
+            var f7 = this;
+            if (f7.device.cordova) {
+                // if (f7.device.android) {
+                // Init cordova APIs (see cordova-app.js)
+                cordovaApp.init(f7);
+            }
+        },
+    },
 });
 // Login Screen Demo
 $('#my-login-screen .login-button').on('click', function () {
-  var username = $('#my-login-screen [name="username"]').val();
-  var password = $('#my-login-screen [name="password"]').val();
+    var username = $('#my-login-screen [name="username"]').val();
+    var password = $('#my-login-screen [name="password"]').val();
 
-  // Close login screen
-  app.loginScreen.close('#my-login-screen');
+    // Close login screen
+    app.loginScreen.close('#my-login-screen');
 
-  // Alert username and password
-  app.dialog.alert('Username: ' + username + '<br/>Password: ' + password);
+    // Alert username and password
+    app.dialog.alert('Username: ' + username + '<br/>Password: ' + password);
 });
